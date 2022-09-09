74d7d259ae7e42b37512a1d423cc30a23b483975
Caleb Flenoury <cflenour@usc.edu>
2022-09-09 09:08:19 -0700

    Uploading current progress

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -2,6 +2,10 @@
 #include <stdexcept>
 #include "ulliststr.h"
 
+////clean up
+#include <iostream>
+////
+
 ULListStr::ULListStr()
 {
   head_ = NULL;
@@ -25,6 +29,62 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+  void ULListStr::push_front(const std::string& val){
+    //Is this initial push (first item in list)?
+    if(empty()){
+      head_ = new Item;
+      head_->val[0] = val;
+      head_->last = 1;
+      tail_ = head_;
+    }else{
+      //Check if there is room before the first string in the head node
+      if(head_->first != 0){//If there is room before the first string
+        head_->val[head_->first - 1] = val;//Assign val to the space before the first string in array
+        head_->first--;//Set first to now be one index less
+      }else if (head_->first == 0){//If there is no room before the first string create a new node and store the value there
+        Item* temp = new Item;
+        temp->val[ARRSIZE-1] = val;
+        temp->next = head_;//Make new node point to old head
+        head_ = temp;//Make head_ point to this new node
+        head_->first = ARRSIZE-1;//Set first of this node to the index where the string was placed (end of array)
+        head_->last = ARRSIZE;    //Set last to the place after where the string was placed (ARRSIZE)
+      }
+    }    
+
+    //Increment size
+    size_++;
+    //std::cout << "The size is now " << size_ << std::endl;
+    //std::cout <<"\"" << head_->val[head_->first]<<"\" is now at the front of the list\n";
+
+  }
+
+void ULListStr::push_back(const std::string& val){
+  if(empty()){
+    tail_ = new Item;
+    tail_->val[0] = val;
+    tail_->last = 1;
+    head_ = tail_;
+  }else{
+    //Is there room in the tail_ for another item?
+    if(tail_->last == ARRSIZE){//tail_ is full
+      //std::cout << "Attempting to create new item\n";
+      Item* temp = new Item;
+      temp->val[0] = val;
+      temp->prev = tail_;
+      tail_->next = temp;
+      tail_ = temp;
+      tail_->last=1;
+      //std::cout << "First value in new item is " << tail_->val[0] <<std::endl;
+    }else{//tail_ has space for more values
+      //Place  val in "last" index
+      tail_->val[tail_->last] = val;
+
+      //Increment "last" index
+      tail_->last++;
+    }
+  }
+  size_++;//increment size_
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
@@ -62,4 +122,44 @@ void ULListStr::clear()
   }
   tail_ = NULL;
   size_ = 0;
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+  //std::cout << "Getting value...\n";
+  //Check if loc is valid
+  if(loc < 0 || loc >= size_){
+    return nullptr;
+  }else{//If loc is valid
+    if(loc <= (ARRSIZE - head_->first-1)){ //Is loc in head?
+      //std::cout << (ARRSIZE - head_->first) << " <- should be 2\n";
+      //Item of interest is in the head_
+      return &head_->val[head_->first + loc];
+    }else{//Item is not in head    
+      //std::cout << "last iter?\n";
+      //How many items are in head_? Subtract that from loc
+      int items_in_head = ARRSIZE - head_->first;
+      //std::cout << "There are " << items_in_head << " items in head.\n";
+
+      loc -= (items_in_head);
+
+      //Int divide loc w/ ARRSIZE to find how many nodes ahead of head_ it is in
+      int num_item_jumps = ((int)loc / (int)ARRSIZE) + 1;
+      //std::cout << "You would jump " << num_item_jumps << " times through all items starting at head\n";
+
+      //Go forward in list by number of item jumps
+      Item* temp = head_;
+
+      for(int i = 0; i < num_item_jumps; i++){
+        temp = temp->next;
+      }
+
+      //std::cout << "Returning item at index " << (int)loc % 10 << " of " << num_item_jumps << " (st/nd/rd/th) item after head\n";
+      //std::cout << "That item is " << temp->val[(int)loc % 10] << std::endl;
+      
+      //temp == tail_? std::cout << "we are in the tail\n" : std::cout << "We are not in the tail\n" ;
+      //std::cout << "(T/F) We are in the tail: " << fff  << std::endl;
+
+      return &temp->val[(int)loc % 10];
+    }
+  }
 }
--- ulliststr_test.cpp
+++ ulliststr_test.cpp
@@ -1,13 +1,66 @@
 /* Write your test code for the ULListStr in this file */
-#include "split.h"
 #include "ulliststr.h"
 #include <iostream>
 using namespace std;
 
-void printLists(Node* list);
+////Clean up
+#include <string>
+////
 
 
 int main(int argc, char* argv[])
 {
+  cout << "Starting test\n";
+
+  {
+    ULListStr dat;
+    for(int i = 1; i <= 10; i++){
+      dat.push_front(std::to_string(i));
+    }
+    dat.push_front("hard!");
+    dat.push_front("of");
+    dat.push_front("kind");
+    dat.push_front("is");
+    dat.push_front("104");
+    dat.push_front("CSCI");
+    dat.push_front("cool.");
+    dat.push_front("is");
+    dat.push_front("Caleb");
+
+    cout << "This is the size after using push_front: " << dat.size() << endl;
+
+    for(size_t i = 0; i < dat.size(); i++){
+      cout << dat.get(i) << " ";
+    }
+
+    cout << endl;
+  }
+
+  {
+    ULListStr dat;
+
+    dat.push_back("Caleb");
+    dat.push_back("is");
+    dat.push_back("cool.");
+    dat.push_back("CSCI");
+    dat.push_back("104");
+    dat.push_back("is");
+    dat.push_back("kind");
+    dat.push_back("of");
+    dat.push_back("hard!");
+
+    for(int i = 1; i <= 10; i++){
+      dat.push_back(std::to_string(i));
+    }
+
+    cout << "This is the size after using push_back: " << dat.size() << endl;
+
+    for(size_t i = 0; i < dat.size(); i++){
+      cout << dat.get(i) << " ";
+    }
+
+    cout << endl;
+  }
+
   return 0;
 }
